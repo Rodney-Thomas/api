bdb2f3e2ddeeb6cbd45798ecdfef6da79478b0f8
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-04-09 11:33:30 -0500

    Update api.js

--- api.js
+++ api.js
@@ -2,8 +2,8 @@ let date = Date();
 
 function student() {
   var person = prompt("Please enter your name", "John Doe");
-  if (person != null) {
-    document.getElementById("demo").innerHTML =
-    "Hello " + person + "! How are you today? Today's date is" + date;
+  if(person !== "John Doe") {
+    alert('Welcome' + person)
+  }
   }
 }
