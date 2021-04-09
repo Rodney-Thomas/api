26c71806d4690a1d306309b38ea5072064bb67f3
Rodney-Thomas <80913258+Rodney-Thomas@users.noreply.github.com>
2021-04-09 11:34:27 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,6 +3,6 @@ let date = Date();
 function student() {
   var person = prompt("Please enter your name", "John Doe");
   if(person !== "John Doe") {
-    alert('Welcome' + person)
+    alert('Welcome ' + person)
   }
 }
